dab1cdffefe70ecc0e53711d62a5528d10c59408
Thierry Libeau <thierry.libeau@gmail.com>
2023-11-03 15:42:09 +0100

    patching form error with number

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  form: UntypedFormGroup;
 
-  constructor() {}
-
+  constructor(    public formBuilder: UntypedFormBuilder,
+    ) {
+      this.form = this.formBuilder.group({
+          segment: [3],
+    });
+      this.form.patchValue({segment:"3"})
+  }
 }
